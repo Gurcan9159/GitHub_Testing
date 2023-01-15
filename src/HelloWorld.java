a296fff09d1ffd781fb162ac817cfbc8d96b975a
Kudret A <kudretnew@gmail.com>
2023-01-15 13:49:27 +0100

    age added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
             String hello = "Hello World";
+            int age= 45;
+        System.out.println("I am " + age + " years old");
             System.out.println("This is my message to the globe: " + hello);
 
         }
