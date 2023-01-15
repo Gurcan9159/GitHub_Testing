656365253d678d26502681e5aaee1ec1cac12b28
Kudret A <kudretnew@gmail.com>
2023-01-15 13:40:14 +0100

    message added

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+            String hello = "Hello World";
+            System.out.println("This is my message to the globe: " + hello);
+
+        }
+    }
+
