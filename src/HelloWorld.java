4665d1c0cd12d8fbdd94327f14884d371c59f79c
Gurcan9159 <114821525+Gurcan9159@users.noreply.github.com>
2023-01-16 13:51:29 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-            String hello = "Hello World";
+            String hello = "Hello World Whats Up";
             int age= 45;
-        System.out.println("I am " + age + " years old");
+        System.out.println("We are " + age + " years old");
             System.out.println("This is my message to the globe: " + hello);
 
         }
